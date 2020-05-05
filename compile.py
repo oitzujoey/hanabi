350ff9d5d369b55a6d2abeba69b75fa2ec774f0d
Ethan Hill <ethan.hill1999@outlook.com>
2020-05-04 20:39:27 -0500

    new bits

--- compile.py
+++ compile.py
@@ -11,10 +11,31 @@ import os, pprint
 1 - spot
 2 - add
 3 - visitnotequal
+4 - visitequal
 """
 
 asci = ''
-data = [int(t) for t in "0 4 0 0 0 0 0 0 1 0 0 0 0 1 0 4 0 4 1 1 2 0 4 5 1 1 1 3".split(' ')]
+data = [
+    0, 4, 0, 0, 0, 0, 0,    # a(4) = 0
+    1, 4, 1, 0, 0, 0, 0,    # b(1) = 0
+    0, 1, 0, 0, 0, 0, 1,    # spot A
+    0, 4, 0, 4, 1, 1, 2,    # a = a + 1
+    1, 4, 0, 4, 1, 1, 2,
+    0, 4, 5, 1, 1, 1, 3     # bne a, 5 goto A
+]
+
+tt = 9
+t_pos = 0
+
+def new_t():
+    t_pos = (t_pos + 1) % tt
+    return t_pos
+
+def interpret(s1, s2):
+    if s2 == 4:
+        return 't' + str(s1)
+    else:
+        return str(s1)
 
 pre = ''
 fin = ''
@@ -33,15 +54,21 @@ while pos < len(data):
     o   = data[pos+6]
 
     if o == 0:
-        if y1 == 0 and y2 == 0:
-            fin += pre + 'la t' + str(t) + ', 0;\n'
+        if y2 == 0:
+            fin += pre + 'la t' + str(x1) + ', ' + str(y1) + ';\n'
+        elif y2 == 1:
+            fin += pre + 'move t' + str(x1) + ', ' + interpret(y1,y2) + ';\n'
     if o == 1:
         fin += chr(65 + x1) + ':\n'
         pre = '\t'
     if o == 2:
-        fin += pre + 'add t' + str(x1) + ', t' + str(y1) + ', ' + str(z1) + ';\n'
+        x = interpret(x1, x2)
+        y = interpret(y1, y2)
+        fin += pre + 'add ' + x + ', ' + y + ', ' + str(z1) + ';\n'
     if o == 3:
-        fin += pre + 'bne t' + str(x1) + ', ' + str(y1) + ', ' + chr(65 + x1) + ';'
+        x = interpret(x1, x2)
+        y = interpret(y1, y2)
+        fin += pre + 'bne ' + x + ', ' + y + ', ' + chr(65 + x1) + ';'
 
     pos += 7
 
